d37e4ed017f3693a5028b37fd014f3e393d29ec1
Aleksandr Chupakhin <sanarise@yandex.ru>
2018-04-18 02:50:43 +0300

    Add Calc and Even games instances

---
+++ src/games/calc.js
@@ -0,0 +1,33 @@
+import { createGame } from '../game';
+import { createTask } from '../task';
+import { randomIntInRange } from '../math';
+
+const GAME_RULES = 'What is the result of the expression?';
+const MIN_NUM = 0;
+const MAX_NUM = 99;
+
+export default createGame(GAME_RULES, () => {
+  const num1 = randomIntInRange(MIN_NUM, MAX_NUM);
+  const num2 = randomIntInRange(MIN_NUM, MAX_NUM);
+
+  let question;
+  let answer;
+  const operatorNum = randomIntInRange(0, 2);
+  switch (operatorNum) {
+    case 0:
+      question = `${num1} + ${num2}`;
+      answer = num1 + num2;
+      break;
+    case 1:
+      question = `${num1} - ${num2}`;
+      answer = num1 - num2;
+      break;
+    case 2:
+      question = `${num1} * ${num2}`;
+      answer = num1 * num2;
+      break;
+    default:
+  }
+
+  return createTask(question, answer);
+});
