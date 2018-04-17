8d5c95b9eb644a7120b1cfbc723599b8b5176a52
Aleksandr Chupakhin <sanarise@yandex.ru>
2018-04-18 02:52:49 +0300

    Add common script for one game with no mistakes

---
+++ src/scripts/one-game-with-no-mistakes.js
@@ -0,0 +1,21 @@
+import runGame from '../game-runer';
+import { getRules } from '../game';
+import * as io from '..';
+
+export default (game, questionLimit = 3) => {
+  io.postWelcome();
+  io.postString(getRules(game));
+  io.postEmtyLine();
+
+  const userName = io.askUserName();
+  io.postUserGreeting(userName);
+  io.postEmtyLine();
+
+  const mistakes = runGame(game)(questionLimit);
+
+  if (mistakes > 0) {
+    io.postTryAgain(userName);
+  } else {
+    io.postFinalCongrats(userName);
+  }
+};
