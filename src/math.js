072a11a83275158f0a9d2c94cd211276f78e88c6
Aleksandr Chupakhin <sanarise@yandex.ru>
2018-04-18 19:29:55 +0300

    Replace gcd realisation to Euclidean algorithm

--- src/math.js
+++ src/math.js
@@ -2,15 +2,4 @@ export const randomIntInRange = (min, max) => Math.floor(Math.random() * ((max -
 
 export const isEven = int => int % 2 === 0;
 
-export const gcd = (x, y) => {
-  let a = x;
-  let b = y;
-  while (a !== b) {
-    if (a > b) {
-      a -= b;
-    } else {
-      b -= a;
-    }
-  }
-  return a;
-};
+export const gcd = (x, y) => (!y ? x : gcd(y, x % y));
