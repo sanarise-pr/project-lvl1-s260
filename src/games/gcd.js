397804280a0848291e9a519b110634cb58660be3
Aleksandr Chupakhin <sanarise@yandex.ru>
2018-04-18 13:46:57 +0300

    Move gcd function to math module

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,27 +1,14 @@
 import { createGame } from '../game';
 import { createTask } from '../task';
-import { randomIntInRange } from '../math';
+import { randomIntInRange, gcd } from '../math';
 
 const GAME_RULES = 'Find the greatest common divisor of given numbers.';
 const MIN_NUM = 1;
 const MAX_NUM = 100;
 
-const calcGcd = (x, y) => {
-  let a = x;
-  let b = y;
-  while (a !== b) {
-    if (a > b) {
-      a -= b;
-    } else {
-      b -= a;
-    }
-  }
-  return a;
-};
-
 export default createGame(GAME_RULES, () => {
   const num1 = randomIntInRange(MIN_NUM, MAX_NUM);
   const num2 = randomIntInRange(MIN_NUM, MAX_NUM);
 
-  return createTask(`${num1} ${num2}`, calcGcd(num1, num2));
+  return createTask(`${num1} ${num2}`, gcd(num1, num2));
 });
--- src/math.js
+++ src/math.js
@@ -1,3 +1,16 @@
 export const randomIntInRange = (min, max) => Math.floor(Math.random() * ((max - min) + 1)) + min;
 
 export const isEven = int => int % 2 === 0;
+
+export const gcd = (x, y) => {
+  let a = x;
+  let b = y;
+  while (a !== b) {
+    if (a > b) {
+      a -= b;
+    } else {
+      b -= a;
+    }
+  }
+  return a;
+};
